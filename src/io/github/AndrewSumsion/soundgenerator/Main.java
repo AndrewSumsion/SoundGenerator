66ab0370520d491e3e6d4153dd6190d6ce237142
Andrew Sumsion <sumsiand000@alpine.local>
2018-09-14 10:22:35 -0600

    commit

--- src/io/github/AndrewSumsion/soundgenerator/Main.java
+++ src/io/github/AndrewSumsion/soundgenerator/Main.java
@@ -1,6 +1,7 @@
 package io.github.AndrewSumsion.soundgenerator;
 
 import io.github.AndrewSumsion.soundgenerator.valuecontrollers.LFO;
+import io.github.AndrewSumsion.soundgenerator.valuecontrollers.PanController;
 import io.github.AndrewSumsion.soundgenerator.valuecontrollers.StaticValue;
 
 import javax.sound.sampled.AudioFormat;
@@ -13,7 +14,8 @@ public class Main {
         SoundPipeline pipeline = new SoundPipeline(44100, 2048);
         Recorder recorder = new Recorder();
         pipeline.add(new SineGenerator(new StaticValue(440)));
-        pipeline.add(new FrequencyModulator(new StaticValue(880F), new LFO(new StaticValue(0F), new StaticValue(3F), new StaticValue(0.5F), 44100),  false));
+        pipeline.add(new FrequencyModulator(new StaticValue(440F), new LFO(new StaticValue(1F), new StaticValue(3F), new PanController(2F, 5F, 10F, 44100), 44100),  false));
+        //pipeline.add(new Amplifier(new LFO(new StaticValue(0.5F), new StaticValue(1F), new PanController(2F, 6F, 10F, 44100), 44100)));
         pipeline.add(recorder);
         pipeline.run(44100 * 10 / 2048);
         System.out.println("Done. Exporting.");
